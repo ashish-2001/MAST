1a8de8b886453a7f2c24e193611e8e7760a8d58a
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2026-01-12 18:02:09 +0530

    Initial commit

---
+++ packages/database/index.js
@@ -0,0 +1,47 @@
+import mongoose from 'mongoose';
+
+const UserSchema = new mongoose.Schema({
+
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    phoneNumber: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    gender: {
+        type: String,
+        required: true
+    },
+    dateOfBirth: {
+        type: Date,
+        required: true
+    },
+    lastLogin: {
+        type: Date,
+        default: Date.now
+    }
+    
+}, { timestamps: true });
+
+const User = mongoose.model('User', UserSchema);
+
+export {
+    userModel
+}
