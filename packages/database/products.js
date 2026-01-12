ef66daeed43813367074c4a1309f50fc52033a91
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2026-01-12 18:45:16 +0530

    Added product

---
+++ packages/database/products.js
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+
+const productsSchema = new mongoose.Schema({
+
+    productName: {
+        type: String,
+        required: true
+    },
+
+    productDescription: {
+        type: String,
+        required: true
+    },
+
+    productsPrice: {
+        type: Number,
+        required: true
+    },
+
+    thumbnailImage: {
+        type: String,
+        required: true
+    },
+
+    productStock: {
+        type: Number,
+        required: true
+    },
+
+    categories: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Category'
+    }
+});
+
+const Product = mongoose.model('Product', productsSchema);
+
+export {
+    Product
+}
