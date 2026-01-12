c33fd307ee14c596068a762e9fdff46d8d54fe43
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2026-01-12 18:31:04 +0530

    Added address

---
+++ packages/database/address.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const AddressSchema = new mongoose.Schema({
+    address: {
+        type: String,
+        required: true
+    },
+    landmark: {
+        type: String,
+        required: true
+    },
+    city: {
+        type: String,
+        required: true
+    },
+    state: {
+        type: String,
+        required: true
+    },
+    pinCode: {
+        type: Number,
+        required: true
+    }
+});
+
+const Address = mongoose.model("Address", AddressSchema);
+
+export {
+    Address
+}
