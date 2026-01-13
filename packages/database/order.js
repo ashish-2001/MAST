77701f9ce487fccda969a6968fba0e6f59be9aec
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2026-01-13 18:41:46 +0530

    Added order

--- packages/database/order.js
+++ packages/database/order.js
@@ -1,8 +1,39 @@
 import mongoose from "mongoose";
 
 const orderSchema = new mongoose.Schema({
-
-});
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    items: [
+        {
+            product: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'Product',
+                required: true
+            },
+            quantity: {
+                type: Number,
+                required: true,
+                min: 1
+            },
+            priceAtPurchase: {
+                type: Number,
+                required: true
+            }
+        }
+    ],
+    totalAmount: {
+        type: Number,
+        required: true
+    },
+    orderStatus: {
+        type: String,
+        enum: ['Pending', 'Paid', 'Shipped', 'Delivered', 'Cancelled'],
+        required: true
+    }
+}, { timestamps: true });
 
 const Order = mongoose.model('Order', orderSchema);
 
