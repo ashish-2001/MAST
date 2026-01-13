9670090c25f3b2c9b81810b246f7188420cc4fd1
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2026-01-13 18:54:39 +0530

    Added category

---
+++ packages/database/category.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+
+const categorySchema = new mongoose.Schema({
+
+    categoryName: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    categoryDescription: {
+        type: String,
+        required: true
+    },
+    categoryThumbnail: {
+        type: String,
+        required: true
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }
+}, { timestamps: true });
+
+const Category = mongoose.model('Category', categorySchema);
+
+export {
+    Category
+}
