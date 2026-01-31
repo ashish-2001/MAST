9399227ec051c7f0d6f8303ddb87ec7711d2cb5f
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2026-01-31 15:47:22 +0530

    Added user schema

---
+++ packages/shared/schemas/userSchema.js
@@ -0,0 +1,19 @@
+const signUpValidator = z.object({
+    firstName: z.string().min(3, "First name should not be less than three words"),
+    lastName: z.string().min(3, "Last name should not be less than three words"),
+    phoneNumber: z.number().min(10, "Phone number should not be less than 10 digits").max(10, "Phone number should not be more than 10 digits"),
+    email: z.string().email("Invalid email address"),
+    password: z.string().min(8, "Password should not be less than 8 letters").max(16, "Password should not be more than 16 letters"),
+    confirmPassword: z.string().min(8, "Confirm password should not be less than 8 letters").max(16, "Confirm password should not be more than 16 letters"),
+    otp: z.string().min(6, "Otp is too short").max(6, "Otp is Invalid")
+});
+
+const signInValidator = z.object({
+    email: z.string().email("Invalid email address"),
+    password: z.string().min(8, "Password is too short").max(16, "Invalid password")
+});
+
+export {
+    signUpValidator,
+    signInValidator
+}
