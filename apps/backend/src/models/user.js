ef73acfc6495489d2e849782129e62e77ba6b4d8
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2026-01-31 15:02:16 +0530

    Added user model

--- apps/backend/src/models/user.js
+++ apps/backend/src/models/user.js
@@ -35,7 +35,12 @@ const UserSchema = new mongoose.Schema({
             product: {
                 type: mongoose.Schema.Types.ObjectId,
                 ref:'Product'
-            }
+            },
+            quantity: {
+                type: Number,
+                default: 1,
+                min: 1
+            } 
         }
     ],
     wishlist: [
