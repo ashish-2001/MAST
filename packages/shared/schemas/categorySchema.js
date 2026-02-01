27265edb84593cdeafff384af5207825a5476a85
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2026-02-01 17:10:55 +0530

    Added category schema

--- packages/shared/schemas/categorySchema.js
+++ packages/shared/schemas/categorySchema.js
@@ -4,4 +4,8 @@ const categorySchema = z.object({
     categoryName: z.string().min(3, "Category name is too short!").max(60, "Category name required!"),
     categoryDescription: z.string().min(3, "Category description is too short").max(200, "Category description is required!"),
     categoryThumbnailImage: z.url(1, "Category image url is invalid!")
-})
+});
+
+export {
+    categorySchema
+};
