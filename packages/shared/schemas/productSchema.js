0b53bef58569b7818b0ef4dce84080336a131be4
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2026-02-01 15:26:41 +0530

    Added product schema

---
+++ packages/shared/schemas/productSchema.js
@@ -0,0 +1,7 @@
+const productValidator = z.object({
+    productName: z.string().min(3, "Product name is too small").max(50, 'Product name should not be greater than 50 words!'),
+    productDescription: z.string().min(3, "Product description is too small").max(200, "Product description should not be greater than 200 words!"),
+    productPrice: z.number("Product price must be a number").positive("Product price must be greater than 0"),
+    thumbnailImage: z.url("Product image must be a valid url"),
+    productStock: z.int("Product stock must be an integer").nonnegative("Product stock can not negative!")
+});
