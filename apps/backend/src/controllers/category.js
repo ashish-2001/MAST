185954ecbe5344e8b4327c08617c36d6d20a73ed
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2026-02-02 16:23:13 +0530

    Added category controller

--- apps/backend/src/controllers/category.js
+++ apps/backend/src/controllers/category.js
@@ -1,5 +1,6 @@
 import { categoryValidator } from "../../../../packages/shared/schemas/categorySchema";
 import { Category } from "../models/category";
+import { Product } from "../models/products";
 import { User } from "../models/user";
 
 async function createCategory(req, res){
@@ -76,4 +77,143 @@ async function createCategory(req, res){
             message: e.message
         });
     };
+};
+
+async function addProductToCategory(req, res){
+
+    const userId = req.user.userId;
+    const { productId, categoryId } = req.params;
+
+    try{
+        const product = await Product.findById(productId);
+
+        if(!product){
+            return res.status(404).json({
+                success: false,
+                message: "Product not found!"
+            });
+        };
+
+        const category = await Category.findById(categoryId);
+
+        if(!category){
+            return res.status(404).json({
+                success: false,
+                message: "Category not found!"
+            });
+        };
+
+        const user = await User.findById(userId);
+
+        if(!user){
+            return req.status(404).json({
+                success: false,
+                message: "User not found!"
+            });
+        };
+
+        if(category.products.includes(productId)){
+            return res.status(300).json({
+                success: false,
+                message: 'Product already exists in the category!'
+            });
+        };
+
+        category.products.push(productId);
+        await category.save();
+
+        return res.status(200).json({
+            success: false,
+            message: "Product added to the category!"
+        });
+    } catch(e){
+        return res.status(500).json({
+            success: false,
+            message: e.message
+        });
+    };
+};
+
+async function editCategory(req, res){
+
+    const userId = req.user.userId;
+    const categoryId = req.params;
+
+    try{
+        const parsedResult = categoryValidator.safeParse(req.body);
+
+        if(parsedResult.success){
+            return res.status(401).json({
+                success: false,
+                message: "All fields are required!"
+            });
+        };
+
+        const { categoryName, categoryDescription } = parsedResult.data;
+
+        const category = await Category.findById(categoryId);
+
+        if(!category){
+            return res.status(404).json({
+                success: false,
+                message: "Category not found!"
+            });
+        };
+
+        const user = await User.findById(userId);
+
+        if(!user){
+            return res.status(404).json({
+                success: false,
+                message: "User not found!"
+            });
+        };
+
+        if(req.files || req.file.thumbnailImage){
+            const thumbnailImage = req.files.thumbnailImage;
+
+            const uploadedImage = await uploadImageToCloudinary(
+                thumbnailImage,
+                process.env.FOLDER_NAME
+            )
+
+            category.thumbnailImage = uploadedImage.secure_url
+        };
+
+        category.categoryName = categoryName;
+        category.categoryDescription = categoryDescription;
+
+        category.save();
+
+
+        const updatedCategory = await Category.findByIdAndUpdate({ _id: categoryId }).populate('user');
+
+        if(!updatedCategory){
+            return res.status(404).json({
+                success: false,
+                message: "Category couldn't be updated successfully!"
+            });
+        };
+
+        return res.status(200).json({
+            data: updatedCategory,
+            success: false,
+            message: "Category updated successfully!"
+        });
+
+    } catch(e){
+        return res.status(500).json({
+            success: false,
+            message: e.message
+        });
+    };
+};
+
+
+export {
+    createCategory,
+    addProductToCategory,
+    editCategory,
+    deleteCategory,
+    getAllCategory
 };
