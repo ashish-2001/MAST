7d696f4acbb23f34e9507b9b56102c6f6d140f64
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2026-02-02 15:40:47 +0530

    Added category schema

--- packages/shared/schemas/categorySchema.js
+++ packages/shared/schemas/categorySchema.js
@@ -1,11 +1,11 @@
 import z from "zod";
 
-const categorySchema = z.object({
+const categoryValidator = z.object({
     categoryName: z.string().min(3, "Category name is too short!").max(60, "Category name required!"),
     categoryDescription: z.string().min(3, "Category description is too short").max(200, "Category description is required!"),
     categoryThumbnailImage: z.url(1, "Category image url is invalid!")
 });
 
 export {
-    categorySchema
+    categoryValidator
 };
