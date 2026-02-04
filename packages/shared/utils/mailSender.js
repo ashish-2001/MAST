a26d6c2456c07851baaa05865e3cfee949c62788
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2026-02-04 17:13:56 +0530

    Initial commit

---
+++ packages/shared/utils/mailSender.js
@@ -0,0 +1,30 @@
+import nodemailer from "nodemailer";
+
+async function mailSender(email, title, body){
+    try{
+        let transporter = nodemailer,createTransport({
+            host: process.env.SMTP_HOST,
+            port: process.env.SMTP_PORT,
+            auth: {
+                user: process.env.MAIL_USER,
+                pass: process.env.MAIL_PASS
+            },
+            secure: false
+        });
+
+        const info = await transporter.sendMail({
+            from: `MAST <${process.env.MAIL_USER}>`,
+            to: `${email}`,
+            subject: `${title}`,
+            html: `${body}`
+        });
+
+        return info;
+    } catch(e){
+        return e.message
+    };
+};
+
+export { 
+    mailSender
+}
