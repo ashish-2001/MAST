df7d98658ec05827c768fb4ac7aa410c757c3ac0
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2026-02-04 16:27:54 +0530

    Added cloudinary config

---
+++ packages/shared/config/cloudinary.js
@@ -0,0 +1,18 @@
+import { cloudinary } from "cloudinary";
+
+
+function cloudinaryConnect(){
+    try{
+        cloudinary.config({
+            cloud_name: process.env.CLOUD_NAME,
+            api_key: process.env.API_KEY,
+            api_secret: process.env.API_SECRET
+        });
+    }catch(e){
+        console.log(e.message);
+    }
+};
+
+export {
+    cloudinaryConnect
+};
