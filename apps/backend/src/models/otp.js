ee22d4f3468bb834145e5b4aa4cea5ac2c5fb00f
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2026-02-04 16:10:25 +0530

    Added otp model

--- apps/backend/src/models/otp.js
+++ apps/backend/src/models/otp.js
@@ -14,8 +14,35 @@ const optSchema = new mongoose.Schema({
         type: String,
         enum: ['Customer', 'Admin'],
         required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        expires: 60 * 5
     }
 }, { timestamps: true });
+
+async function sendVerificationEmail(email, otp){
+    try{
+        const mailResponse = await mailSender(
+            email,
+            "Verification Email",
+            otpTemplate(otp)
+        );
+        console.log("Email response", mailResponse);
+    } catch(e){
+        throw new error("Failed to send verification email", e.message);
+    };
+};
+
+optSchema.pre("save", async function(next){
+    console.log("New document saved in the database");
+
+    if(this.isNew){
+        await sendVerificationEmail(this.email, this.otp);
+    }
+    next();
+});
 
 const Otp = mongoose.model('Otp', optSchema);
 
