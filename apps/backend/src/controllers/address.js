57ad2bf175d1e66d10726ab85d3416efa2765128
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2026-02-04 14:27:42 +0530

    Added address controller

--- apps/backend/src/controllers/address.js
+++ apps/backend/src/controllers/address.js
@@ -58,6 +58,72 @@ async function createAddress(req, res){
     }
 };
 
+async function editAddress(req, res){
+    const userId = req.user.userId;
+    const addressId = req.params;
+
+    try{
+
+        const parsedResult = addressValidator.safeParse(req.body);
+
+        if(!parsedResult.success){
+            return res.status(403).json({
+                success: false,
+                message: "All fields are required!"
+            });
+        };
+
+        const { address, landmark, city, state, pinCode } = parsedResult.data;
+
+        const user = await User.findById(userId);
+
+        if(!user){
+            return res.status(404).json({
+                success: false,
+                message: "User not found!"
+            });
+        };
+
+        const addresses = await Address.findById(addressId);
+
+        if(!addresses){
+            return res.status(404).json({
+                success: false,
+                message: "Address not found!"
+            });
+        };
+
+        addresses.address = address;
+        addresses.landMark = landmark;
+        addresses.city = city;
+        addresses.state = state;
+        addresses.pinCode = pinCode;
+
+        addresses.save();
+
+        const updatedAddress = await Address.findById({ _id: addressId });
+
+        if(!updatedAddress){
+            return res.status(404).json({
+                success: false,
+                message: "Address couldn't be updated!"
+            });
+        };
+
+        return res.status(200).json({
+            data: updatedAddress,
+            success: true,
+            message: "Address updated successfully!"
+        });
+
+    }catch(e){
+        return res.status(500).json({
+            success: false,
+            message: e.message
+        })
+    }
+}
+
 export {
     createAddress,
     editAddress,
