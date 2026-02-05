c4872ba8f856a0ff7061d5c83b7a2e569f11b949
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2026-02-05 18:32:03 +0530

    Added payment route

--- apps/backend/src/routes/paymentRoute.js
+++ apps/backend/src/routes/paymentRoute.js
@@ -1,7 +1,11 @@
+import { capturePayment, verifyPaymentAndCreateOrder } from "../controllers/payment";
 import { auth, isCustomer, IsAdmin } from "../middleware/auth";
 import express, { Router } from "express";
 
 const router = express.Router();
+
+router.post("/capturePayment", auth, isCustomer, capturePayment);
+router.post("/verifyPayment", auth, IsAdmin, verifyPaymentAndCreateOrder)
 
 export {
     router
