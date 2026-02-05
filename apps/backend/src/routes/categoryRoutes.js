d780f0e243b135da82f4094530ff1ab9dc44539c
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2026-02-05 18:09:35 +0530

    Added category route

--- apps/backend/src/routes/categoryRoutes.js
+++ apps/backend/src/routes/categoryRoutes.js
@@ -5,7 +5,7 @@ import express from 'express';
 const router = express.Router();
 
 router.post("/createCategory", auth, IsAdmin, createCategory);
-router.get("/geAllCategories", auth, getAllCategory);
+router.get("/getAllCategories", auth, getAllCategory);
 router.put("/updateCategory/:categoryId", auth, IsAdmin, editCategory);
 router.delete("/deleteCategory/:categoryId", auth, IsAdmin, deleteCategory);
 router.post("/addProductToCategory", auth, IsAdmin, addProductToCategory);
