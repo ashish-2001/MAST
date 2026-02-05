e428f3d65f91987f1fa2eea5749ccf569fe64682
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2026-02-05 18:29:21 +0530

    Added user route

--- apps/backend/src/routes/userRoute.js
+++ apps/backend/src/routes/userRoute.js
@@ -1,7 +1,13 @@
-import { auth, IsAdmin, isCustomer } from "../middleware/auth";
+import { changePassword, sendOtp, signin, signup } from "../controllers/Auth";
+import { auth } from "../middleware/auth";
 import express from "express";
 
 const router = express.Router();
+
+router.post("/signup", signup);
+router.post("/login", signin);
+router.post("/sendOtp", sendOtp);
+router.put("/changePassword", auth, changePassword);
 
 export {
     router
