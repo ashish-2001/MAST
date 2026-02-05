c4694bce0ad147cecc94b24436f3060110ce71e8
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2026-02-05 14:34:14 +0530

    Added mail sender

--- packages/shared/utils/mailSender.js
+++ packages/shared/utils/mailSender.js
@@ -1,22 +1,23 @@
 import nodemailer from "nodemailer";
 
+const transporter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    auth: {
+        user: process.env.MAIL_USER,
+        pass: process.env.MAIL_PASS
+    },
+    secure: false
+});
+
 async function mailSender(email, title, body){
     try{
-        let transporter = nodemailer.createTransport({
-            host: process.env.SMTP_HOST,
-            port: process.env.SMTP_PORT,
-            auth: {
-                user: process.env.MAIL_USER,
-                pass: process.env.MAIL_PASS
-            },
-            secure: false
-        });
-
         const info = await transporter.sendMail({
-            from: `MAST <${process.env.MAIL_USER}>`,
-            to: `${email}`,
-            subject: `${title}`,
-            html: `${body}`
+            from,
+            to,
+            subject,
+            html,
+            replyTo
         });
 
         return info;
