7f8d177e25c451695725d019984e161920bb4fdb
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2026-02-05 17:52:21 +0530

    Added contact route

--- apps/backend/src/routes/contactRoute.js
+++ apps/backend/src/routes/contactRoute.js
@@ -1,10 +1,14 @@
-import { contactUs } from "../controllers/contact";
+import { contactUs, deleteContactMessage, getContactMessagesByStatus, updateContactMessageStatus } from "../controllers/contact";
 import express from "express";
+import { auth, IsAdmin, isCustomer } from "../middleware/auth";
 
 
 const router = express.Router();
 
-router.post("/contactUs", contactUs);
+router.post("/contactUs", auth, isCustomer, contactUs);
+router.delete("/deleteContactMessage", auth, IsAdmin, deleteContactMessage);
+router.put("/updateContactMessageStatus", auth, IsAdmin, updateContactMessageStatus);
+router.get("/getContactMessagesByStatus", auth, IsAdmin, getContactMessagesByStatus);
 
 export { 
     router
