e4c583e9caf012c93e701deca29c4f2bdec94ff6
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2026-02-05 17:00:04 +0530

    Added contact message sent successful email

---
+++ apps/backend/src/services/mailTemplates/contactMessageSentSuccessfulEmail.js
@@ -0,0 +1,11 @@
+function contactMessageSentSuccessfulEmail(){
+    return (
+        `<p>
+            This is a contact message sent successful email
+        </p>`
+    )
+}
+
+export {
+    contactMessageSentSuccessfulEmail
+}
