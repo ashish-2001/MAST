a7243e8dc5b43fa83d01b681ec308a8a9f1cdb38
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2026-02-06 17:07:03 +0530

    Added database config

--- packages/shared/config/database.js
+++ packages/shared/config/database.js
@@ -3,11 +3,15 @@ import mongoose from "mongoose";
 async function connect(){
 
     try{
-        await mongoose.connect(process.env.MONGO_URL, {
+        const mongoUrl = await mongoose.connect(process.env.MONGO_URL, {
             useNewUrlParser: true,
             useUnifiedTopology: true
         });
         console.log("Database connected!");
+
+        if(!mongoUrl){
+            console.log("Mongo url is undefined!");
+        }
     } catch(e){
         console.log(e);
         process.exit(1);
