571d750760f73bcfd3fd9193500848554e92e428
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2026-02-07 15:53:12 +0530

    Added footer

--- packages/ui/src/components/Footer.jsx
+++ packages/ui/src/components/Footer.jsx
@@ -1,42 +1,35 @@
-function Footer({ footerData, onClick, searchesData }){
+function Footer({ footerData, socialMediaData, footerSearchesData }){
     return (
         <div>
             <div>
-                <div>
+                {footerData.map((data) => (
 
-                </div>
-                <div>
-
-                </div>
-                <div>
-                    
-                </div>
-                <div>
-                    
-                </div>
+                    <div key={data.id}>
+                        <p>{footerData.title}</p>
+                        <Link navigate={footerData.link}>{footerData.services}</Link>
+                    </div>
+                ))}
             </div>
             <hr/>
             <div>
-
+                {socialMediaData.map((mediaData) => (
+                    <Link navigate={socialMediaData.link} key={mediaData.id}>
+                        {socialMediaData.link}
+                    </Link>
+                ))}
             </div>
             <div>
-                <h1></h1>
-                <div>
-                    <h4></h4>
-                    <p></p>
-                    <hr/>
-                </div>
-            </div>
-            <div>
-                <p>
-
-                </p>
-                <p>
-
-                </p>
-                <p>
-
-                </p>
+                {footerSearchesData.map((searchData) => (
+                    <div key={searchData.id}>
+                        <p><b>{footerSearchesData.title}</b></p>
+                        <div>
+                            <p><b>{footerSearchesData.category}</b></p>
+                            <div>
+                                <Link navigate={footerSearchesData.link}>{footerSearchesData.products} | </Link>
+                            </div>
+                        </div>
+                    </div>
+                ))}
             </div>
         </div>
     )
