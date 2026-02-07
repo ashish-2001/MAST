9c2c8f5e43d75a086793883e7eb537b5bf6b591e
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2026-02-07 15:09:56 +0530

    Added useclickoutside

--- packages/hooks/useClickOutside.jsx
+++ packages/hooks/useClickOutside.jsx
@@ -12,7 +12,7 @@ function useClickOutside(ref, handler){
 
         document.addEventListener.("mousedown", listener);
         document.addEventListener("touchstart", listener);
-
+``
         return () => {
             document.removeEventListener("mousedown", listener);
             document.removeEventListener("touchstart", listener);
