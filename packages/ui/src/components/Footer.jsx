c8f91ae8b568cb202f58ddc073c3d798062c5653
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2026-02-07 15:29:44 +0530

    Added Footer common components

--- packages/ui/src/components/Footer.jsx
+++ packages/ui/src/components/Footer.jsx
@@ -1,7 +1,47 @@
-function Footer(){
+function Footer({ footerData, onClick, searchesData }){
     return (
         <div>
-            This is a footer page.
+            <div>
+                <div>
+
+                </div>
+                <div>
+
+                </div>
+                <div>
+                    
+                </div>
+                <div>
+                    
+                </div>
+            </div>
+            <hr/>
+            <div>
+
+            </div>
+            <div>
+                <h1></h1>
+                <div>
+                    <h4></h4>
+                    <p></p>
+                    <hr/>
+                </div>
+            </div>
+            <div>
+                <p>
+
+                </p>
+                <p>
+
+                </p>
+                <p>
+
+                </p>
+            </div>
         </div>
     )
+};
+
+export {
+    Footer
 }
