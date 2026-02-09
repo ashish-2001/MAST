c8310d1872b6e2d6cd25dd491f4c0a6c927e862a
Ashish Pal <95185364+ashish-2001@users.noreply.github.com>
2026-02-09 15:44:41 +0530

    Added navbar links

---
+++ packages/ui/src/data/NavbarLinks.jsx
@@ -0,0 +1,30 @@
+const NavbarLinks = [
+    {
+        title: "New Arrivals",
+        path: "/products/new-arrivals"
+    },
+    {
+        title: "Gifts",
+        path: "/products/gifts"
+    },
+    {
+        title: "Fine Silver",
+        path: "/products/fine-silver"
+    },
+    {
+        title: "Fine Gold",
+        path: "/products/fine-gold"
+    },
+    {
+        title: "About us",
+        path: "/about"
+    },
+    {
+        title: "Customers Favorites",
+        path: "/products/customers-favorites"
+    }
+];
+
+export {
+    NavbarLinks
+};
